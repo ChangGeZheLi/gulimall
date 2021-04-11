7fef19e18ff63c79d744252ee3eed4e58edd8b95
admin <admin>
2021-04-11 17:12:16 +0800

    项目初始结构创建

---
+++ gulimall-product/src/test/java/com/syong/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syong.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
