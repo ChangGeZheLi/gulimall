3bf107a61f7afcb2502755244fc8e93d440fbfea
admin <admin>
2021-04-14 10:57:32 +0800

    添加gateway网关；

---
+++ gulimall-gateway/src/main/java/com/syong/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.syong.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 使用gateway网关
+ * 1、
+ **/
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) //排除数据源自动配置
+@EnableDiscoveryClient
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
